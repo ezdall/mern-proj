36a341f94b557f4f6199395144d6ab93374c002e
ezdall <67018806+ezdall@users.noreply.github.com>
2023-10-10 18:14:10 +0800

    add Prefetch comp to initialize state, add setupListener

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Public from './components/public.comp';
 //
 import Login from './features/login.comp';
 import Welcome from './features/welcome.comp';
+import Prefetch from './features/prefetch.comp';
 //
 import UsersList from './features/users/users-list.comp';
 import NewUserForm from './features/users/new-user-form.comp';
@@ -23,19 +24,21 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-          {/* /dash/notes/:noteId  */}
-          <Route path="notes">
-            <Route index element={<NoteList />} />
-            <Route path="new" element={<NewNote />} />
-            <Route path=":id" element={<EditNote />} />
-          </Route>
-          {/* /dash/users/:userId  */}
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path="new" element={<NewUserForm />} />
-            <Route path=":id" element={<EditUser />} />
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+            {/* /dash/notes/:noteId  */}
+            <Route path="notes">
+              <Route index element={<NoteList />} />
+              <Route path="new" element={<NewNote />} />
+              <Route path=":id" element={<EditNote />} />
+            </Route>
+            {/* /dash/users/:userId  */}
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path="new" element={<NewUserForm />} />
+              <Route path=":id" element={<EditUser />} />
+            </Route>
           </Route>
         </Route>
       </Route>
---
+++ src/features/prefetch.comp.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { store } from '../api/store';
+import { notesApiSlice } from './notes/notesApiSlice';
+import { usersApiSlice } from './users/usersApiSlice';
+
+export default function Prefetch() {
+  useEffect(() => {
+    console.log('subscribing');
+
+    // fill-up users when in /notes route
+    const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate());
+    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+
+    return () => {
+      console.log('UNsubscribing');
+      notes.unsubscribe();
+      users.unsubscribe();
+    };
+  }, []);
+
+  return <Outlet />;
+}
