dbb67ededa378205b6cbc78172db1f180c39f404
ezdall <67018806+ezdall@users.noreply.github.com>
2023-10-12 21:47:46 +0800

    remove redudant 'import React', add Link to /new

--- src/features/notes/new-note.comp.jsx
+++ src/features/notes/new-note.comp.jsx
@@ -1,12 +1,10 @@
 import { useSelector } from 'react-redux';
-import { selectAllUsers } from '../users/usersApiSlice';
 
+import { selectAllUsers } from '../users/usersApiSlice';
 import NewNoteForm from './new-note-form.comp';
 
 export default function NewNote() {
   const users = useSelector(selectAllUsers);
-
-  // console.log('users:', users);
 
   if (users?.length) return <NewNoteForm users={users} />;
 
